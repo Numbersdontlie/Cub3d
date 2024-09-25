950346d58ddc11d27d56ea8064f74e0c01311a05
Numbersdontlie <luifer92@gmail.com>
2024-09-25 14:44:39 +0200

    fixing compilation errors

--- sources/raycasting/renderV2.c
+++ sources/raycasting/renderV2.c
@@ -6,7 +6,7 @@
 /*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 23:25:59 by luifer            #+#    #+#             */
-/*   Updated: 2024/09/25 12:07:00 by luifer           ###   ########.fr       */
+/*   Updated: 2024/09/25 14:02:27 by luifer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@
 //region and draw the ceiling or floor color if no texture is present
 void	ft_update_pixels_img(t_data *data, t_img *img, int x, int y)
 {
-	if (data->texture_pixels[y][x] > 0)
-		ft_put_pixel_to_img(img, x, y, data->texture_pixels[y][x]);
+	int	pix;
+
+	pix = data->texture_pixels[y][x];
+	if (pix > 0)
+		ft_put_pixel_to_img(img, x, y, pix);
 	else if (y < HEIGHT / 2)
 		ft_put_pixel_to_img(img, x, y, data->textinfo->hex_ceiling);
 	else if (y < HEIGHT - 1)
@@ -68,7 +71,7 @@ void	ft_render_ray(t_data *data)
 //has moved and update the player new position accordingly
 //the raycast is render after this verification to display the new
 //view perspective or keep the current one
-void	ft_render(t_data *data)
+int	ft_render(t_data *data)
 {
 	int	move;
 
