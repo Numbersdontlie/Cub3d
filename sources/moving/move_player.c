03c1882c1e2f6622903cb7e360731e8a2942e263
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2024-10-10 10:22:25 +0200

    steps are happening but it isnt smooth

--- sources/moving/move_player.c
+++ sources/moving/move_player.c
@@ -6,7 +6,7 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 23:59:22 by luifer            #+#    #+#             */
-/*   Updated: 2024/10/10 10:10:49 by kbolon           ###   ########.fr       */
+/*   Updated: 2024/10/10 10:16:23 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ void	ft_rotation(t_data *data)
 //player x and y position after movement. It will add the x_dir to pos_x 
 //to update new x position. It will add dir_y to pos_y to update new y position
 //it will call the validate movement function to check if move is valid in the map.
-int	ft_move_player_fw(t_data *data)
+int	ft_move_player_fw(t_data *data)//move_player_forward
 {
 	double	x_after;
 	double	y_after;
@@ -90,7 +90,7 @@ int	ft_move_player_fw(t_data *data)
 //player x and y position after movement. It will substract the x_dir from pos_x 
 //to update new x position. It will substract dir_y from pos_y to update new y position
 //it will call the validate movement function to check if move is valid in the map.
-int	ft_move_player_bw(t_data *data)
+int	ft_move_player_bw(t_data *data)//move_player_backward
 {
 	double	x_after;
 	double	y_after;
@@ -110,7 +110,7 @@ int	ft_move_player_bw(t_data *data)
 //player x and y position after movement. It will add the y_dir to pos_x 
 //to update new x position. It will substract dir_y from pos_y to update new y position
 //it will call the validate movement function to check if move is valid in the map.
-int	ft_move_player_left(t_data *data)
+int	ft_move_player_left(t_data *data)//move_player_left
 {
 	double	x_after;
 	double	y_after;
@@ -130,7 +130,7 @@ int	ft_move_player_left(t_data *data)
 //player x and y position after movement. It will substract the y_dir from pos_x 
 //to update new x position. It will add dir_x to pos_x to update new y position
 //it will call the validate movement functionto check if move is valid in the map.
-int	ft_move_player_right(t_data *data)
+int	ft_move_player_right(t_data *data)//move_player_right
 {
 	double	x_after;
 	double	y_after;
@@ -151,7 +151,7 @@ int	ft_move_player_right(t_data *data)
 //if movement is backward or forward. it will check if move_x
 //is 1 or -1 to check if movement is left or right. It will return
 //the move number
-int	ft_move_player(t_data *data)
+int	ft_move_player(t_data *data)//move_player
 {
 	int	move;
 
